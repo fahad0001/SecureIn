68f213eb532f189f375307d3be6a654de874dda1
Fahad Mir <fahadmir0001@hotmail.com>
2017-03-13 15:45:45 +0500

    database file configuaration change

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'qasim.chnrhqillmoo.us-west-2.rds.amazonaws.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'securein'),
+            'username' => env('DB_USERNAME', 'qasimali0001'),
+            'password' => env('DB_PASSWORD', 'harry1234'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
